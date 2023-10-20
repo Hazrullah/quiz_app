573fecd2dc63618af11d46db9821f80a22b8957c
Hazrullah <hazrullah27@gmail.com>
2023-10-21 00:06:55 +0800

    refactor

--- lib/widgets/container_card.dart
+++ lib/widgets/container_card.dart
@@ -1,17 +1,19 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class ContainerCard extends StatelessWidget {
   final double width;
   final double height;
   final String text;
+  final void Function()? onTap;
 
-  const ContainerCard({super.key, this.width = double.infinity, this.height = 50, required this.text});
+  const ContainerCard({super.key, this.width = double.infinity, this.height = 50, required this.text, this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-
+        onTap;
       },
       child: Container(
         width: width,
@@ -29,8 +31,9 @@ class ContainerCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(8),
         ),
         child: Center(
-          child: Text(
+          child: AutoSizeText(
             text,
+            minFontSize: 20,
             style: TextStyle(
               color: Theme.of(context).colorScheme.onPrimaryContainer,
             ),
