a26d38272e54c713dbccab111234b158344ce47f
Hazrullah <hazrullah27@gmail.com>
2023-12-20 10:20:52 +0800

    delete file

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quiz_app/providers/fill_in_the_blank_provider.dart';
-import 'package:quiz_app/screens/matching_1.dart';
-import 'package:quiz_app/screens/matching_2.dart';
-import 'package:quiz_app/screens/multiple_choice_1.dart';
-import 'package:quiz_app/screens/multiple_choice_2.dart';
-import 'package:quiz_app/screens/true_false.dart';
 import 'package:quiz_app/theme_manager.dart';
 import 'screens/fill_in_blank.dart';
-import 'screens/learning_section.dart';
 
 void main() {
   runApp(const MainApp());
@@ -29,12 +23,6 @@ class MainApp extends StatelessWidget {
         themeMode: ThemeMode.light,
         home:
         FillInBlank(),
-        // TrueFalse(),
-        // MultipleChoice1(),
-        // MultipleChoice2(),
-        // Matching1(),
-        // Matching2(),
-        // LearningSection(),
       ),
     );
   }
